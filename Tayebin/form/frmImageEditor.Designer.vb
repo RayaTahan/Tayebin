df1719ffb6fb145f71bd4835cf20eb1d7bb2059b
PurTahan <sina.farahabadi@gmail.com>
2018-10-16 07:49:49 +0330

    اضافه شدن امکان برش #10

--- Tayebin/form/frmImageEditor.Designer.vb
+++ Tayebin/form/frmImageEditor.Designer.vb
@@ -25,23 +25,34 @@ Partial Class frmImageEditor
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmImageEditor))
         Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
         Me.panel = New System.Windows.Forms.Panel()
+        Me.Abzar = New System.Windows.Forms.GroupBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.lblTaqir = New System.Windows.Forms.Label()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.btnOK = New System.Windows.Forms.Button()
         Me.preview = New System.Windows.Forms.PictureBox()
         Me.btnTaeed = New System.Windows.Forms.Button()
         Me.btnEnseraf = New System.Windows.Forms.Button()
-        Me.Button1 = New System.Windows.Forms.Button()
         Me.picPanel = New System.Windows.Forms.Panel()
         Me.pic = New System.Windows.Forms.PictureBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
         Me.TableLayoutPanel1.SuspendLayout()
         Me.panel.SuspendLayout()
+        Me.Abzar.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
         CType(Me.preview, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.picPanel.SuspendLayout()
         CType(Me.pic, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox2.SuspendLayout()
         Me.SuspendLayout()
         '
         'TableLayoutPanel1
         '
         Me.TableLayoutPanel1.ColumnCount = 2
-        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150.0!))
         Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
         Me.TableLayoutPanel1.Controls.Add(Me.panel, 0, 0)
         Me.TableLayoutPanel1.Controls.Add(Me.picPanel, 1, 0)
@@ -55,25 +66,108 @@ Partial Class frmImageEditor
         '
         'panel
         '
-        Me.panel.Controls.Add(Me.preview)
-        Me.panel.Controls.Add(Me.btnTaeed)
-        Me.panel.Controls.Add(Me.btnEnseraf)
-        Me.panel.Controls.Add(Me.Button1)
+        Me.panel.Controls.Add(Me.Abzar)
+        Me.panel.Controls.Add(Me.GroupBox1)
+        Me.panel.Controls.Add(Me.GroupBox2)
         Me.panel.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.panel.Location = New System.Drawing.Point(610, 3)
+        Me.panel.Location = New System.Drawing.Point(580, 3)
         Me.panel.Name = "panel"
         Me.panel.Padding = New System.Windows.Forms.Padding(7)
-        Me.panel.Size = New System.Drawing.Size(114, 465)
+        Me.panel.Size = New System.Drawing.Size(144, 465)
         Me.panel.TabIndex = 0
+        '
+        'Abzar
+        '
+        Me.Abzar.Controls.Add(Me.Panel1)
+        Me.Abzar.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Abzar.Location = New System.Drawing.Point(7, 189)
+        Me.Abzar.Name = "Abzar"
+        Me.Abzar.Size = New System.Drawing.Size(130, 159)
+        Me.Abzar.TabIndex = 5
+        Me.Abzar.TabStop = False
+        Me.Abzar.Text = "ابزار"
+        '
+        'Panel1
+        '
+        Me.Panel1.AutoScroll = True
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel1.Location = New System.Drawing.Point(3, 25)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(124, 131)
+        Me.Panel1.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Button1.Location = New System.Drawing.Point(0, 0)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(124, 40)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "برش"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.lblTaqir)
+        Me.GroupBox1.Controls.Add(Me.btnCancel)
+        Me.GroupBox1.Controls.Add(Me.btnOK)
+        Me.GroupBox1.Controls.Add(Me.preview)
+        Me.GroupBox1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.GroupBox1.Location = New System.Drawing.Point(7, 7)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(130, 182)
+        Me.GroupBox1.TabIndex = 4
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "تغییرات"
+        '
+        'lblTaqir
+        '
+        Me.lblTaqir.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.lblTaqir.Location = New System.Drawing.Point(3, 26)
+        Me.lblTaqir.Name = "lblTaqir"
+        Me.lblTaqir.Size = New System.Drawing.Size(124, 29)
+        Me.lblTaqir.TabIndex = 7
+        Me.lblTaqir.Text = "lblTaqir"
+        Me.lblTaqir.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnCancel
+        '
+        Me.btnCancel.BackColor = System.Drawing.Color.Red
+        Me.btnCancel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.btnCancel.FlatAppearance.BorderColor = System.Drawing.Color.Green
+        Me.btnCancel.FlatAppearance.BorderSize = 0
+        Me.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnCancel.Location = New System.Drawing.Point(5, 1)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(22, 22)
+        Me.btnCancel.TabIndex = 6
+        Me.btnCancel.UseVisualStyleBackColor = False
+        Me.btnCancel.Visible = False
+        '
+        'btnOK
+        '
+        Me.btnOK.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btnOK.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.btnOK.FlatAppearance.BorderColor = System.Drawing.Color.Green
+        Me.btnOK.FlatAppearance.BorderSize = 0
+        Me.btnOK.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnOK.Location = New System.Drawing.Point(35, 1)
+        Me.btnOK.Name = "btnOK"
+        Me.btnOK.Size = New System.Drawing.Size(22, 22)
+        Me.btnOK.TabIndex = 5
+        Me.btnOK.UseVisualStyleBackColor = False
+        Me.btnOK.Visible = False
         '
         'preview
         '
         Me.preview.Dock = System.Windows.Forms.DockStyle.Bottom
-        Me.preview.Location = New System.Drawing.Point(7, 278)
+        Me.preview.Location = New System.Drawing.Point(3, 55)
         Me.preview.Name = "preview"
-        Me.preview.Size = New System.Drawing.Size(100, 100)
+        Me.preview.Size = New System.Drawing.Size(124, 124)
         Me.preview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
-        Me.preview.TabIndex = 3
+        Me.preview.TabIndex = 4
         Me.preview.TabStop = False
         '
         'btnTaeed
@@ -83,11 +177,11 @@ Partial Class frmImageEditor
         Me.btnTaeed.Enabled = False
         Me.btnTaeed.FlatAppearance.BorderColor = System.Drawing.Color.Green
         Me.btnTaeed.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.btnTaeed.Location = New System.Drawing.Point(7, 378)
+        Me.btnTaeed.Location = New System.Drawing.Point(3, 27)
         Me.btnTaeed.Name = "btnTaeed"
-        Me.btnTaeed.Size = New System.Drawing.Size(100, 40)
+        Me.btnTaeed.Size = New System.Drawing.Size(124, 40)
         Me.btnTaeed.TabIndex = 2
-        Me.btnTaeed.Text = "تایید"
+        Me.btnTaeed.Text = "ذخیره و خروج"
         Me.btnTaeed.UseVisualStyleBackColor = False
         '
         'btnEnseraf
@@ -95,25 +189,14 @@ Partial Class frmImageEditor
         Me.btnEnseraf.BackColor = System.Drawing.Color.Red
         Me.btnEnseraf.DialogResult = System.Windows.Forms.DialogResult.Cancel
         Me.btnEnseraf.Dock = System.Windows.Forms.DockStyle.Bottom
-        Me.btnEnseraf.Enabled = False
         Me.btnEnseraf.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
         Me.btnEnseraf.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.btnEnseraf.Location = New System.Drawing.Point(7, 418)
+        Me.btnEnseraf.Location = New System.Drawing.Point(3, 67)
         Me.btnEnseraf.Name = "btnEnseraf"
-        Me.btnEnseraf.Size = New System.Drawing.Size(100, 40)
+        Me.btnEnseraf.Size = New System.Drawing.Size(124, 40)
         Me.btnEnseraf.TabIndex = 1
         Me.btnEnseraf.Text = "انصراف"
         Me.btnEnseraf.UseVisualStyleBackColor = False
-        '
-        'Button1
-        '
-        Me.Button1.Dock = System.Windows.Forms.DockStyle.Top
-        Me.Button1.Location = New System.Drawing.Point(7, 7)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(100, 40)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "برش"
-        Me.Button1.UseVisualStyleBackColor = True
         '
         'picPanel
         '
@@ -122,7 +205,7 @@ Partial Class frmImageEditor
         Me.picPanel.Dock = System.Windows.Forms.DockStyle.Fill
         Me.picPanel.Location = New System.Drawing.Point(3, 3)
         Me.picPanel.Name = "picPanel"
-        Me.picPanel.Size = New System.Drawing.Size(601, 465)
+        Me.picPanel.Size = New System.Drawing.Size(571, 465)
         Me.picPanel.TabIndex = 1
         '
         'pic
@@ -134,6 +217,18 @@ Partial Class frmImageEditor
         Me.pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
         Me.pic.TabIndex = 4
         Me.pic.TabStop = False
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.btnTaeed)
+        Me.GroupBox2.Controls.Add(Me.btnEnseraf)
+        Me.GroupBox2.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.GroupBox2.Location = New System.Drawing.Point(7, 348)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(130, 110)
+        Me.GroupBox2.TabIndex = 6
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "خروجی"
         '
         'frmImageEditor
         '
@@ -150,19 +245,30 @@ Partial Class frmImageEditor
         Me.Text = "ویراستار تصویر"
         Me.TableLayoutPanel1.ResumeLayout(False)
         Me.panel.ResumeLayout(False)
+        Me.Abzar.ResumeLayout(False)
+        Me.Panel1.ResumeLayout(False)
+        Me.GroupBox1.ResumeLayout(False)
         CType(Me.preview, System.ComponentModel.ISupportInitialize).EndInit()
         Me.picPanel.ResumeLayout(False)
         CType(Me.pic, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox2.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
     Friend WithEvents panel As Panel
-    Friend WithEvents Button1 As Button
     Friend WithEvents btnTaeed As Button
     Friend WithEvents btnEnseraf As Button
-    Friend WithEvents preview As PictureBox
     Friend WithEvents picPanel As Panel
     Friend WithEvents pic As PictureBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents btnCancel As Button
+    Friend WithEvents btnOK As Button
+    Friend WithEvents preview As PictureBox
+    Friend WithEvents lblTaqir As Label
+    Friend WithEvents Abzar As GroupBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button1 As Button
+    Friend WithEvents GroupBox2 As GroupBox
 End Class
--- Tayebin/form/frmImageEditor.vb
+++ Tayebin/form/frmImageEditor.vb
@@ -1,8 +1,8 @@
 ﻿Imports System.Drawing.Drawing2D
-Imports Kaliko.ImageLibrary
 
 Public Class frmImageEditor
     Dim fileAddress As String
+    Dim Job As Integer = 0 ' 1:crop 
 
     Dim tempCnt As Boolean         'check weather the roller is used or not
 
@@ -43,7 +43,8 @@ Public Class frmImageEditor
     End Sub
 
     Private Sub frmImageEditor_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+        btnOK.BackgroundImage = IMGcache.img($"{Application.StartupPath}\data\app\icon\tick11.png")
+        btnCancel.BackgroundImage = IMGcache.img($"{Application.StartupPath}\data\app\icon\cross106.png")
     End Sub
 
     Private Sub resizer()
@@ -76,41 +77,45 @@ Public Class frmImageEditor
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        If preview.Image IsNot Nothing Then
-            pic.Image = preview.Image
-            pic.BackColor = Color.Pink
-            resizer()
-        End If
+        Job = 1
+        lblTaqir.Text = "برش"
+        Abzar.Enabled = False
+        btnOK.Visible = True
+        btnCancel.Visible = True
+        btnTaeed.Enabled = False
     End Sub
 
 
     Private Sub pic_MouseDown(sender As Object, e As MouseEventArgs) Handles pic.MouseDown
         Try
-            If e.Button = Windows.Forms.MouseButtons.Left Then
-                cropX = e.X
-                cropY = e.Y
+            If Job = 1 Then
+                If e.Button = Windows.Forms.MouseButtons.Left Then
+                    cropX = e.X
+                    cropY = e.Y
 
-                cropPen = New Pen(cropPenColor, cropPenSize)
-                cropPen.DashStyle = DashStyle.DashDotDot
-                Cursor = Cursors.Cross
+                    cropPen = New Pen(cropPenColor, cropPenSize)
+                    cropPen.DashStyle = DashStyle.DashDotDot
+                    Cursor = Cursors.Cross
+                End If
+                pic.Refresh()
             End If
-            pic.Refresh()
         Catch exc As Exception
         End Try
     End Sub
 
     Private Sub pic_MouseMove(sender As Object, e As MouseEventArgs) Handles pic.MouseMove
         Try
-            If pic.Image Is Nothing Then Exit Sub
+            If Job = 1 Then
+                If pic.Image Is Nothing Then Exit Sub
 
-            If e.Button = Windows.Forms.MouseButtons.Left Then
-                isStart = True
-                'If e.X < pic.Left Or e.X > pic.Left + pic.Width Or e.Y < pic.Top Or e.Y > pic.Top + pic.Height Then Exit Sub
-                If Not MouseIsOverControl(pic) Then Exit Sub
-                pic.Refresh()
-                cropWidth = Math.Max(e.X, cropX) - Math.Min(e.X, cropX)
-                cropHeight = Math.Max(e.Y, cropY) - Math.Min(e.Y, cropY)
-                pic.CreateGraphics.DrawRectangle(cropPen, Math.Min(cropX, e.X), Math.Min(cropY, e.Y), cropWidth, cropHeight)
+                If e.Button = Windows.Forms.MouseButtons.Left Then
+                    isStart = True
+                    If Not MouseIsOverControl(pic) Then Exit Sub
+                    pic.Refresh()
+                    cropWidth = Math.Max(e.X, cropX) - Math.Min(e.X, cropX)
+                    cropHeight = Math.Max(e.Y, cropY) - Math.Min(e.Y, cropY)
+                    pic.CreateGraphics.DrawRectangle(cropPen, Math.Min(cropX, e.X), Math.Min(cropY, e.Y), cropWidth, cropHeight)
+                End If
             End If
         Catch exc As Exception
             If Err.Number = 5 Then Exit Sub
@@ -122,39 +127,74 @@ Public Class frmImageEditor
 
     Private Sub pic_MouseUp(sender As Object, e As MouseEventArgs) Handles pic.MouseUp
         Try
-            Cursor = Cursors.Default
-            Try
-                If cropWidth < 10 Or cropHeight < 10 Or Not isStart Then
-                    pic.Refresh()
-                    Exit Sub
-                End If
-                isStart = False
+            If Job = 1 Then
+                Cursor = Cursors.Default
+                Try
+                    If cropWidth < 10 Or cropHeight < 10 Or Not isStart Then
+                        pic.Refresh()
+                        Exit Sub
+                    End If
+                    isStart = False
 
-                Dim rect As Rectangle = New Rectangle(Math.Min(cropX, e.X), Math.Min(cropY, e.Y), cropWidth, cropHeight)
-                Dim bit As Bitmap = New Bitmap(pic.Image, pic.Width, pic.Height)
+                    Dim rect As Rectangle = New Rectangle(Math.Min(cropX, e.X), Math.Min(cropY, e.Y), cropWidth, cropHeight)
+                    Dim bit As Bitmap = New Bitmap(pic.Image, pic.Width, pic.Height)
 
-                'cropBitmap = New Bitmap(cropWidth, cropHeight)
-                'Dim g As Graphics = Graphics.FromImage(cropBitmap)
-                'g.InterpolationMode = InterpolationMode.HighQualityBicubic
-                'g.PixelOffsetMode = PixelOffsetMode.HighQuality
-                'g.CompositingQuality = CompositingQuality.HighQuality
+                    'cropBitmap = New Bitmap(cropWidth, cropHeight)
+                    'Dim g As Graphics = Graphics.FromImage(cropBitmap)
+                    'g.InterpolationMode = InterpolationMode.HighQualityBicubic
+                    'g.PixelOffsetMode = PixelOffsetMode.HighQuality
+                    'g.CompositingQuality = CompositingQuality.HighQuality
 
-                'g.DrawImage(bit, 0, 0, rect, GraphicsUnit.Pixel)
-                'preview.Image = cropBitmap
+                    'g.DrawImage(bit, 0, 0, rect, GraphicsUnit.Pixel)
+                    'preview.Image = cropBitmap
 
-
-                Dim img As New ImageProcessor.ImageFactory()
-                img.Quality(100)
-                img.Load(bit)
-                preview.Image = img.Crop(rect).Image
-
-            Catch exc As Exception
-            End Try
+                    Dim img As New ImageProcessor.ImageFactory()
+                    img.Quality(100)
+                    img.Load(bit)
+                    preview.Image = img.Crop(rect).Image
+                Catch exc As Exception
+                End Try
+            End If
         Catch exc As Exception
         End Try
     End Sub
 
     Private Sub frmImageEditor_Resize(sender As Object, e As EventArgs) Handles Me.Resize
         resizer()
+    End Sub
+
+    Private Sub btnOK_Click(sender As Object, e As EventArgs) Handles btnOK.Click
+        If preview.Image IsNot Nothing Then
+            pic.Image = preview.Image
+            resizer()
+
+            btnOK.Visible = False
+            btnCancel.Visible = False
+            Abzar.Enabled = True
+            btnTaeed.Enabled = True
+
+            Job = 0
+            lblTaqir.Text = ""
+            resizer()
+            pic.Refresh()
+            preview.Image = Nothing
+        End If
+    End Sub
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        Job = 0
+        lblTaqir.Text = ""
+        resizer()
+        pic.Refresh()
+        preview.Image = Nothing
+
+        btnOK.Visible = False
+        btnCancel.Visible = False
+        Abzar.Enabled = True
+        btnTaeed.Enabled = True
+    End Sub
+
+    Private Sub btnTaeed_Click(sender As Object, e As EventArgs) Handles btnTaeed.Click
+
     End Sub
 End Class
